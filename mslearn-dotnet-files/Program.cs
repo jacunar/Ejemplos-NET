fc9826d4c47b906c19957a6aea33897e758d2dfd
jacunar <josueally@gmail.com>
2023-01-14 20:50:15 -0600

    Agregado funcion para guardar archivo de texto que resume las ventas

--- mslearn-dotnet-files/Program.cs
+++ mslearn-dotnet-files/Program.cs
@@ -1,30 +1,51 @@
-﻿using System.IO;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json;
 
 var currentDirectory = Directory.GetCurrentDirectory();
 var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+Directory.CreateDirectory(salesTotalDir);
+
 var salesFiles = FindFiles(storesDirectory);
 
-foreach (var file in salesFiles)
-{
-    Console.WriteLine(file);
-}
+var salesTotal = CalculateSalesTotal(salesFiles);
 
-IEnumerable<string> FindFiles(string folderName)
-{
+File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
+
+Console.WriteLine(salesTotal);
+
+IEnumerable<string> FindFiles(string folderName) {
     List<string> salesFiles = new List<string>();
 
     var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
 
-    foreach (var file in foundFiles)
-    {
+    foreach (var file in foundFiles) {
         var extension = Path.GetExtension(file);
         // The file name will contain the full path, so only check the end of it
-        if (extension == ".json")
-        {
+        if (extension == ".json") {
             salesFiles.Add(file);
         }
     }
 
     return salesFiles;
 }
+
+double CalculateSalesTotal(IEnumerable<string> salesFiles) {
+    double salesTotal = 0;
+
+    // Loop over each file path in salesFiles
+    foreach (var file in salesFiles) {
+        // Read the contents of the file
+        string salesJson = File.ReadAllText(file);
+
+        // Parse the contents as JSON
+        SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+
+        // Add the amount found in the Total field to the salesTotal variable
+        salesTotal += data?.total ?? 0;
+    }
+
+    return salesTotal;
+}
+
+record SalesData (double total);
