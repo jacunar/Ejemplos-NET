73433c88fe2ebd8e6a1550e62b3e027c993e176b
jacunar <josueally@gmail.com>
2023-01-27 16:46:58 -0600

    Agregado almacenamiento SQLite

--- dot-net-api-minima/Program.cs
+++ dot-net-api-minima/Program.cs
@@ -3,9 +3,10 @@ using Microsoft.OpenApi.Models;
 using PizzaStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Pizzas") ?? "Data Source=Pizzas.db";
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
+builder.Services.AddSqlite<PizzaDb>(connectionString);
 builder.Services.AddSwaggerGen(c => {
      c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API", Description = "Making the Pizzas you love", Version = "v1" });
 });
