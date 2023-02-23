00348cd60801a11b77b88bf27841fa8c74fe23be
jacunar <josueally@gmail.com>
2023-02-22 23:57:41 -0600

    Agregado proyecto Repositorio

---
+++ DesignPatterns/DesignPatterns.Repository/Repository.cs
@@ -0,0 +1,33 @@
+﻿using DesignPatterns.Models.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesignPatterns.Repository;
+public class Repository<TEntity> : IRepository<TEntity> where TEntity : class {
+    private DesignPatternsContext _context;
+    private DbSet<TEntity> _dbSet;
+
+    public Repository(DesignPatternsContext context) {
+        _context = context;
+        _dbSet = context.Set<TEntity>();
+    }
+
+    public void Add(TEntity data) => _dbSet.Add(data);
+
+    public void Delete(int id) {
+        var toDelete = _dbSet.Find(id);
+        _dbSet.Remove(toDelete);
+    }
+
+    public IEnumerable<TEntity> Get() => _dbSet.ToList();
+
+    public TEntity Get(int id) {
+        return _dbSet.Find(id); 
+    }
+
+    public void Save() => _context.SaveChanges();
+
+    public void Update(TEntity data) {
+        _dbSet.Attach(data);
+        _context.Entry(data).State = EntityState.Modified;
+    }
+}
