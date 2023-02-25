43ee18cf8e63ddd28c00da92289307dad83bbe0f
jacunar <josueally@gmail.com>
2023-02-24 23:07:38 -0600

    Agregado controlador y vista Beer

---
+++ DesignPatterns/DesignPatternsAsp/Controllers/BeerController.cs
@@ -0,0 +1,23 @@
+﻿using DesignPatterns.Repository;
+using DesignPatternsAsp.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPatternsAsp.Controllers; 
+public class BeerController : Controller {
+    private readonly IUnitOfWork _unitOfWork;
+    
+    public BeerController(IUnitOfWork unitOfWork) {
+        _unitOfWork = unitOfWork;
+    }
+
+    public IActionResult Index() {
+        IEnumerable<BeerViewModel> beers = from d in _unitOfWork.Beers.Get()
+                                           select new BeerViewModel {
+                                               Id = d.BeerId,
+                                               Name = d.Name,
+                                               Style = d.Style
+                                           };
+
+        return View("Index", beers);
+    }
+}
