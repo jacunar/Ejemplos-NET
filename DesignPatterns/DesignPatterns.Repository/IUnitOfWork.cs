aa227befecfede2b4e8f9f6896610974702fa5e9
jacunar <josueally@gmail.com>
2023-02-24 23:05:12 -0600

    Agregado patron repositorio

---
+++ DesignPatterns/DesignPatterns.Repository/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿using DesignPatterns.Models.Data;
+
+namespace DesignPatterns.Repository; 
+public interface IUnitOfWork {
+    public IRepository<Beer> Beers { get; }
+    public IRepository<Brand> Brands { get; }
+    public void Save();
+}
