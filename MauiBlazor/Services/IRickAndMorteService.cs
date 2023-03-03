65a9b29d8f9a03da6be3c3a1255cf3208d00f469
jacunar <josueally@gmail.com>
2023-03-03 16:14:18 -0600

    Ejemplo de aplicación MAUI con Blazor

---
+++ MauiBlazor/Services/IRickAndMorteService.cs
@@ -0,0 +1,6 @@
+﻿using MauiBlazor.Models;
+
+namespace MauiBlazor.Services; 
+public interface IRickAndMortyService {
+    public Task<RickAndMorty> Get();
+}
---
+++ MauiBlazor/Services/RickAndMortyService.cs
@@ -0,0 +1,15 @@
+﻿using MauiBlazor.Models;
+using System.Text.Json;
+
+namespace MauiBlazor.Services;
+public class RickAndMortyService : IRickAndMortyService {
+    const string url = "https://rickandmortyapi.com/api/character";
+    public async Task<RickAndMorty> Get() {
+        var httpClient = new HttpClient();
+        var response = await httpClient.GetAsync(url);
+        var content = await response.Content.ReadAsStringAsync();
+        var rickandmorty = JsonSerializer.Deserialize<RickAndMorty>(content);
+
+        return rickandmorty;
+    }
+}
