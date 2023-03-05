565a5cd685e4b5a71aa73d8dcb31563d06aeb50b
jacunar <josueally@gmail.com>
2023-03-04 22:09:28 -0600

    Agregado funcionalidad para crear beer y brand

--- DesignPatterns/DesignPatternsAsp/Controllers/BeerController.cs
+++ DesignPatterns/DesignPatternsAsp/Controllers/BeerController.cs
@@ -1,6 +1,8 @@
-﻿using DesignPatterns.Repository;
+﻿using DesignPatterns.Models.Data;
+using DesignPatterns.Repository;
 using DesignPatternsAsp.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DesignPatternsAsp.Controllers; 
 public class BeerController : Controller {
@@ -20,4 +22,42 @@ public class BeerController : Controller {
 
         return View("Index", beers);
     }
+
+    [HttpGet]
+    public IActionResult Add() {
+        var brands = _unitOfWork.Brands.Get();
+        ViewBag.Brands = new SelectList(brands, "BrandId", "Name");
+
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Add(FormBeerViewModel formBeerViewModel) {
+        if (!ModelState.IsValid) {
+            var brands = _unitOfWork.Brands.Get();
+            ViewBag.Brands = new SelectList(brands, "BrandId", "Name");
+            return View("Add", formBeerViewModel);
+        }
+
+        var beer = new Beer();
+        beer.Name = formBeerViewModel.Name;
+        beer.Style = formBeerViewModel.Style;
+
+        if(formBeerViewModel is null) {
+            var brand = new Brand();
+            brand.Name = formBeerViewModel.OtherBrand;
+            brand.BrandId = Guid.NewGuid();
+            beer.BrandId = brand.BrandId;
+            _unitOfWork.Brands.Add(brand);
+        } else {
+            beer.BrandId = (Guid)formBeerViewModel.BrandId;
+        }
+
+        _unitOfWork.Beers.Add(beer);
+        _unitOfWork.Save();
+
+        return RedirectToAction("Index");
+    }
 }
+
+// Quedamos en el minuto 10 de la clase 17. Guardando el 2 tablas
